8cd38f8ba0d655f247a295de3b4134b95073436c
brainbreaker <gautamprajapati06@gmail.com>
2016-11-10 10:42:35 +0530

    Ice cream parlour solved 1

---
+++ src/IceCreamParlour.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * Created by brainbreaker on 9/11/16.
+ */
+public class IceCreamParlour {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter value of x: ");
+        int x = scanner.nextInt();
+
+        outerloop:
+        for (int q = 0; q < x; x++) {
+            System.out.print("Enter value of m: ");
+            int m = scanner.nextInt();
+            System.out.print("Enter value of n: ");
+            int n = scanner.nextInt();
+            int a[] = new int[n];
+            for (int w = 0; w < n; w++){
+                System.out.print("Enter values in the array: ");
+                a[w] = scanner.nextInt();
+            }
+
+            for(int i=0; i<n; i++) {
+                for (int j=i; j < n; j++){
+                    if(a[i]+a[j] == m){
+                        System.out.print(i+1 + " ");
+                        System.out.print(j+1);
+                        break outerloop;
+                    }
+                }
+            }
+        }
+    }
+}
